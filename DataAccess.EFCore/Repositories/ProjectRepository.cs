66d52d460e8f98e9342e607372a0a3944fee211e
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-08-27 23:50:31 +0200

    Developer and Project repositories

---
+++ DataAccess.EFCore/Repositories/ProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.EFCore.Repositories
+{
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
+    {
+        public ProjectRepository(ApplicationContext context) : base(context)
+        {
+
+        }
+    }
+}
