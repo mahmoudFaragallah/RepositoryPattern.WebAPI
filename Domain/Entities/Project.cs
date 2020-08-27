550499af3a3131fc7e7a8aa2f674c68107f8223a
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-08-27 21:10:44 +0200

    Add project files.

---
+++ Domain/Entities/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
